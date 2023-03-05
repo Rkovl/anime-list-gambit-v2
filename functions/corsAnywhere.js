027c1c6f26885c47c8dc5eb600af50d0e459cdd9
RKovl <ryan.kovlaske@gmail.com>
2023-03-04 23:10:28 -0800

    Update corsAnywhere.js

--- functions/corsAnywhere.js
+++ functions/corsAnywhere.js
@@ -6,10 +6,27 @@
 //   console.log(`CORS Anywhere proxy server listening on port ${port}`);
 // });
 
-const cors_proxy = require('cors-anywhere');
+// const cors_proxy = require('cors-anywhere');
 
-exports.handler = cors_proxy.createServer({
-    originWhitelist: [], 
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).handler;
+// exports.handler = cors_proxy.createServer({
+//     originWhitelist: [], 
+//     requireHeader: ['origin', 'x-requested-with'],
+//     removeHeaders: ['cookie', 'cookie2']
+// }).handler;
+
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const url = event.queryStringParameters.url;
+  const response = await fetch(url);
+  const data = await response.text();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+    body: data,
+  };
+};
